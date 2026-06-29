ba051e0219f6bc190a4e96cfb0615643621530a4
Mixail-laboratory <spectrafa@gmail.com>
2026-06-29 04:33:56 +0000

    finish dispatcher

--- include/task_dispatcher.hpp
+++ include/task_dispatcher.hpp
@@ -1,20 +1,25 @@
 #pragma once
 
 #include <memory>
+#include <unordered_map>
 
 #include "queue/priority_queue.hpp"
+#include "queue/queue.hpp"
 #include "thread_pool/thread_pool.hpp"
 #include "types.hpp"
 
 namespace dispatcher {
 
 class TaskDispatcher {
-    // здесь ваш код
+    std::shared_ptr<queue::PriorityQueue> queue_;
+    std::unique_ptr<thread_pool::ThreadPool> thread_pool_;
+
 public:
-    TaskDispatcher(size_t thread_count, ?);
+    TaskDispatcher(size_t thread_count,
+                   const std::unordered_map<TaskPriority, queue::QueueOptions> &option = default_config());
 
     void schedule(TaskPriority priority, std::function<void()> task);
-    ~TaskDispatcher();
+    ~TaskDispatcher() = default;
 
 private:
     static std::unordered_map<TaskPriority, queue::QueueOptions> default_config();
--- src/task_dispatcher.cpp
+++ src/task_dispatcher.cpp
@@ -1,7 +1,26 @@
 #include "task_dispatcher.hpp"
+#include "queue/priority_queue.hpp"
+#include "queue/queue.hpp"
+#include "thread_pool/thread_pool.hpp"
+#include "types.hpp"
+#include <cstddef>
+#include <memory>
+#include <unordered_map>
+#include <utility>
 
 namespace dispatcher {
 
-// здесь ваш код
+std::unordered_map<TaskPriority, queue::QueueOptions> TaskDispatcher::default_config() {
+    std::unordered_map<TaskPriority, queue::QueueOptions> config;
+    config[TaskPriority::High] = queue::QueueOptions(true, 1000);
+    config[TaskPriority::Normal] = queue::QueueOptions(false);
+    return config;
+}
 
-} // namespace dispatcher
+TaskDispatcher::TaskDispatcher(size_t thread_count, const std::unordered_map<TaskPriority, queue::QueueOptions> &option)
+    : queue_(std::make_shared<queue::PriorityQueue>(option)),
+      thread_pool_(std::make_unique<thread_pool::ThreadPool>(thread_count, queue_)) {}
+
+void TaskDispatcher::schedule(TaskPriority priority, std::function<void()> task) { queue_->push(priority, task); }
+
+}  // namespace dispatcher
